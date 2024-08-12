2c31b1d172af4d6831e7fc623a1740c79190dae6
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2024-08-12 08:18:49 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,19 +1,19 @@
 # (c) @AbirHasan2005
 
 import asyncio
+from os import environ
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
+DELETE_TIME = int(environ.get('DELETE_TIME', 120))#1min=60s , 2min=60×2=120 , 5min=60×5=300 😎🤏
 
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**Here is Sharable Link of this file:**\n"
-            f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(file_id))}\n\n"
-            f"__To Retrive the Stored File, just open the link!__",
+            f"**Files will be Deleted After {DELETE_TIME} seconds ⏰**\n",
             disable_web_page_preview=True, quote=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
@@ -33,7 +33,14 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
         return media_forward(bot, user_id, file_id)
 
 
+async def delete_file(file_id: int):
+    await asyncio.sleep(DELETE_TIME)  #1min=60s , 2min=60×2=120 , 5min=60×5=300 😎🤏
+    # Delete the file using the file ID
+    # Code to delete the file goes here
+
+
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
+    asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
     await asyncio.sleep(2)
